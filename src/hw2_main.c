c64cdff290ed7087d2e33804bdbedb866e329feb
Yusuf Fakhriddinov <135459719+yusuffakhriddinov@users.noreply.github.com>
2024-03-04 04:26:37 +0000

    Your commit message here

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -551,15 +551,30 @@ int main(int argc, char **argv) {
         }
         
     } else if(strcmp(input_extension, "sbu")==0 && strcmp(output_extension, "sbu")==0){
-        printf("Both is SBU");
-        copyFile(fp1, fp2);
+        
+        if(cflag==1 && pflag==1){
+            copyPastePPMtoPPM(fp1, fp2, copy, paste);
+            printf("Done");  
+        }else{
+            copyFile(fp1, fp2);
+        }
         
     } else if(strcmp(input_extension, "sbu")==0 && strcmp(output_extension, "ppm")==0){
-        printf("Input is sbu & output is ppm");
-        convertSBUtoPPM(fp1, fp2);
+        if(cflag==1 && pflag==1){
+            copyPastePPMtoPPM(fp1, fp2, copy, paste);
+            printf("Done");  
+        }else{
+            convertSBUtoPPM(fp1, fp2);
+        }
+        
     } else{
-        printf("Input is ppm & output is sbu");
-        convertPPMtoSBU(fp1, fp2);
+        if(cflag==1 && pflag==1){
+            copyPastePPMtoPPM(fp1, fp2, copy, paste);
+            printf("Done");  
+        }else{
+            convertPPMtoSBU(fp1, fp2);
+        }
+        
         
     }
     
