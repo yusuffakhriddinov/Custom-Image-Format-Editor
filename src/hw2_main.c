911f4b26dc542c56d7461d1bec938889e96c07e4
Yusuf Fakhriddinov <135459719+yusuffakhriddinov@users.noreply.github.com>
2024-03-04 15:28:14 +0000

    Your commit message here

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -573,14 +573,14 @@ int main(int argc, char **argv) {
     }
     
     
-    
+    fclose(fp1);
+    fclose(fp2);
+    fclose(fp3);
     
 
     exit(0);
     return 0;
-    // fclose(fp1);
-    // fclose(fp2);
-    // fclose(fp3);
+    
     
 }
 
